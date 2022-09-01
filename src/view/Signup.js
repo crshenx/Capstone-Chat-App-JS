a00cb5323e9ac913a1e691673b9cca701369556a
Chris Hendricks <crshenx@gmail.com>
2022-09-01 12:17:32 -0700

    added sign up func

--- src/view/Signup.js
+++ src/view/Signup.js
@@ -1,7 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 function Signup() {
-  return <div>Signup</div>;
+  const [newUser, setNewUser] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const newUserData = (data, value) => {
+    setNewUser({
+      ...newUser,
+      [data]: value,
+    });
+  };
+
+  function findFormErrors() {
+    const { userName, password, password_confirmation } = newUser;
+    const newErrors = {};
+
+    if (!userName || userName === "") newErrors.name = "must choose username";
+    else if (userName.length > 30 || userName.length < 3)
+      newErrors.userName = "wrong username length";
+    if (password !== password_confirmation)
+      newErrors.password = "password must match confirmation";
+
+    return newErrors;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newErrors = findFormErrors();
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ user: newUser }),
+      })
+        .then((r) => r.json())
+        .then((data) => {
+          // save the token to localStorage for future access
+          // localStorage.setItem("jwt", data.jwt);
+          // save the user somewhere (in state!) to log the user in
+          // setUser(data.user);
+          console.log(data);
+        });
+    }
+  }
+
+  console.log(newUser);
+
+  return (
+    <Form>
+      <Form.Group className="userNameSignup" controlId="formUserName">
+        <Form.Label>User Name</Form.Label>
+        <Form.Control
+          type="username"
+          placeholder="Enter Username"
+          onChange={(e) => newUserData("userName", e.target.value)}
+          isInvalid={!!errors.name}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.name}
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Form.Group className="passwordSignup" controlId="formPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Enter Password"
+          onChange={(e) => newUserData("password", e.target.value)}
+          isInvalid={!!errors.name}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.name}
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Form.Group
+        className="passwordSignupConfirm"
+        controlId="formPasswordConfirm"
+      >
+        <Form.Label>Password Confirmation</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Confirm Password"
+          onChange={(e) => newUserData("password_confirmation", e.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Form>
+  );
 }
 
 export default Signup;

0d8d0750bcd9d7f53d8daf06b308cadcbfd80870
Chris Hendricks <crshenx@gmail.com>
2022-09-02 14:10:53 -0700

    comment cleanup

--- src/view/Signup.js
+++ src/view/Signup.js
@@ -46,24 +46,6 @@ function Signup() {
           console.error(err);
           alert(err);
         });
-      //   fetch("http://localhost:3000/api/v1/users", {
-      //     method: "POST",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //       Accept: "application/json",
-      //     },
-      //     body: JSON.stringify({ user: newUser }),
-      //   })
-      //     .then((r) => r.json())
-      //     .then((data) => {
-      //       // save the token to localStorage for future access
-      //       // localStorage.setItem("jwt", data.jwt);
-      //       // save the user somewhere (in state!) to log the user in
-      //       // setUser(data.user);
-      //       console.log(data);
-      //       navigate("/");
-      //     });
-      // }
     }
   }
 

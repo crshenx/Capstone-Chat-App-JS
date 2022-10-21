5527a0ce6f02f380c322a4a879a8fe3e46a8f617
Chris Hendricks <crshenx@gmail.com>
2022-10-21 16:21:07 -0700

    delete port in url

--- src/channels/consumer.js
+++ src/channels/consumer.js
@@ -2,6 +2,6 @@ import { createConsumer } from "@rails/actioncable";
 import { HOST, PORT, WEBSOCKETHOST } from "../config";
 
 // changed ws to wss for secure socket on heroku
-const wsurl = `wss://${WEBSOCKETHOST}:443/cable`;
+const wsurl = `wss://${WEBSOCKETHOST}/cable`;
 
 export default createConsumer(wsurl);

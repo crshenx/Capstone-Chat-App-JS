f21983c7d4420ebf55e9e1102ff03e8371528bfb
Chris Hendricks <crshenx@gmail.com>
2022-10-24 17:36:09 -0700

    added jwt to ws request

--- src/channels/consumer.js
+++ src/channels/consumer.js
@@ -2,6 +2,8 @@ import { createConsumer } from "@rails/actioncable";
 import { HOST, PORT, WEBSOCKETHOST } from "../config";
 
 // changed ws to wss for secure socket on heroku
-const wsurl = `wss://${WEBSOCKETHOST}/cable`;
+const wsurl = `wss://${WEBSOCKETHOST}/cable?token=${sessionStorage.getItem(
+  "jwt"
+)}`;
 
 export default createConsumer(wsurl);

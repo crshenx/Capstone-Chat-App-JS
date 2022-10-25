523b7d5bce22dda84e52c5be2e36f2d8ec0afd00
Chris Hendricks <crshenx@gmail.com>
2022-10-24 17:48:18 -0700

    revert

--- src/channels/consumer.js
+++ src/channels/consumer.js
@@ -2,8 +2,6 @@ import { createConsumer } from "@rails/actioncable";
 import { HOST, PORT, WEBSOCKETHOST } from "../config";
 
 // changed ws to wss for secure socket on heroku
-const wsurl = `wss://${WEBSOCKETHOST}/cable?token=${sessionStorage.getItem(
-  "jwt"
-)}`;
+const wsurl = `wss://${WEBSOCKETHOST}/cable`;
 
 export default createConsumer(wsurl);

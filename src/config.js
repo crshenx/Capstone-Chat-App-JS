7c0c39d534a8cbc4af2ebde8ea81f543a476084a
Chris Hendricks <crshenx@gmail.com>
2022-10-25 17:38:37 -0700

    deleted comments in config file

--- src/config.js
+++ src/config.js
@@ -1,20 +1,10 @@
 export const AUTH_TOKEN_ID = "jwt";
-// export const protocol = "http";
-// changed to https instead of http
-
-// export const protocol = "https";
-
-//port changed for heroku
-// export const PORT = "3000";
-// hacky and gross
-// export const PORT = "443";
 export const HOST = window.location.host.split(":")[0];
 let PORT = HOST.includes("local") ? 3000 : 443;
 let PROTOCOL = HOST.includes("local") ? "http" : "https";
 let WS_PROTO = HOST.includes("local") ? "ws" : "wss";
 export const WS_BASE_URL = `${WS_PROTO}://${HOST}:${PORT}`;
 export const BASE_URL = `${PROTOCOL}://${HOST}:${PORT}`;
-// export const WEBSOCKETHOST = `capstonechatapp.herokuapp.com`;
 export const API_VER = `/api/v1/`;
 export const LOGIN_ENDPOINT = `${API_VER}login`;
 export const SIGNUP_ENDPOINT = `${API_VER}users`;

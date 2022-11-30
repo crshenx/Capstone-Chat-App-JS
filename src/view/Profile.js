911274f4274882e24b911ea4b64ff9e1a743269f
Logan Figgins <l.figgins.se@gmail.com>
2022-11-29 17:23:25 -0800

    profile

--- src/view/Profile.js
+++ src/view/Profile.js
@@ -6,7 +6,6 @@ import { TextField, Button, IconButton, Box, Avatar } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import EditOffIcon from "@mui/icons-material/EditOff";
 import API from "../client/api";
-import { BASE_URL } from "../config";
 
 function Profile() {
   const navigate = useNavigate();
@@ -89,7 +88,7 @@ function Profile() {
           />
           <Avatar
             alt="Remy Sharp"
-            src={BASE_URL + auth.user.picture || ""}
+            src={auth.user.picture || ""}
             sx={{ width: 56, height: 56 }}
           />
           <Button variant="contained" component="label">
